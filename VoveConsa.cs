50d25e9f8b46e0d7b7e0a1c707ddfbf705830b93
Harry <hareshwarp649>
2022-03-07 07:15:11 +0530

    Check the alphabet is vovel or consonant

---
+++ VoveConsa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    internal class VoveConsa
+    {
+        public static void  CheckVoveCons()
+        {
+            Console.WriteLine("Enter any Character");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'i' || ch == 'o' || ch == 'e' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + "is Vovel");
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
