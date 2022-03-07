98789a7d4bc8d59cc37b83e47a6102205a9bd8e8
Harry <hareshwarp649>
2022-03-07 07:09:07 +0530

    Find the Nth harmonic number

---
+++ ConsoHarmonic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    internal class ConsoHarmonic
+    {
+        public static void ProgramHarmonic()
+        {
+            int K;
+            float harmonic = 1;
+
+            Console.WriteLine("Enter a nth of harmonic number");
+            K = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < K; i++)
+            {
+                harmonic += (float)1 / i;
+            }
+            Console.WriteLine(harmonic);
+           
+        }
+    }
+}
